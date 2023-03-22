23c9214a74d9d70d611f8ce4e8282040fdc51250
Andriy Koval <akoval29@gmail.com>
2023-03-22 14:02:26 +0200

    pre burger

---
+++ script.js
@@ -0,0 +1,50 @@
+"use strict";
+
+// Код, який встановлює на якому саме пристрої було відкрито цей застосунок
+const isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return (
+      navigator.userAgent.match(/IEMobile/i) ||
+      navigator.userAgent.match(/WPDesktop/i)
+    );
+  },
+  any: function () {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  }
+};
+
+// далі код по нашій темі
+if (isMobile.any()) {
+  document.body.classList.add('_touch');
+
+  let menuArrows = document.querySelectorAll('.menu__arrow');
+  if (menuArrows.length > 0) {
+    for (let index = 0; index < menuArrows.length; index++) {
+      const menuArrow = menuArrows[index];
+      menuArrow.addEventListener('click', function (e) {
+        menuArrow.parentElement.classList.toggle('_active');
+      });
+    }
+  }
+
+} else {
+  document.body.classList.add('_pc');
+}
