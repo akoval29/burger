8f6488845f7ccdd2feb7f4a2a01f7a8e1ac48f1d
Andriy Koval <akoval29@gmail.com>
2023-03-22 15:03:13 +0200

    scroll to article

--- script.js
+++ script.js
@@ -44,7 +44,32 @@ if (isMobile.any()) {
       });
     }
   }
-
 } else {
   document.body.classList.add('_pc');
 }
+
+
+// прокрутка до потрібної сторінки
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener("click", onMenuLinkClick);
+  });
+
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth"
+      });
+      e.preventDefault();
+    }
+  }
+}
+
+// в оригінальному тексті було pageYOffset, але це застаріло тому  scrollY
