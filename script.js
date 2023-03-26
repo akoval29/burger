2eefb22671a78894b9b7252236b7f76944adabe4
Andriy Koval <akoval29@gmail.com>
2023-03-27 00:02:51 +0300

    ref

--- script.js
+++ script.js
@@ -49,8 +49,18 @@ if (isMobile.any()) {
 }
 
 
-// прокрутка до потрібної сторінки
+// Меню бургер
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+  iconMenu.addEventListener("Click", function (e) {
+    document.body.classList.toggle('_lock');
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+  });
+}
 
+// прокрутка до потрібної сторінки
 const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
 if (menuLinks.length > 0) {
   menuLinks.forEach(menuLink => {
@@ -62,6 +72,12 @@ if (menuLinks.length > 0) {
     if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
       const gotoBlock = document.querySelector(menuLink.dataset.goto);
       const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+
+      if (iconMenu.classList.contains('.active')) {
+        document.body.classList.remove('_lock');
+        iconMenu.classList.remove('_active');
+        menuBody.classList.remove('_active');
+      }
 
       window.scrollTo({
         top: gotoBlockValue,
